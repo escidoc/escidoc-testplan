bd70adb9a5b7da8260389bcc23fee8cd5dd42505
Matthias Hahn <matthias.hahn@fiz-karlsruhe.de>
2012-08-10 15:09:44 +0200

    maven dependency added

--- run13.sh
+++ run13.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cd measurement
-mvn clean verify -X -Pskip_escidoc_install_13,escidoc_global -Dinstall.home=target/escidoc -Dskip.install.first=false
+#cd measurement
+mvn -f measurement/pom.xml clean verify -X -Pskip_escidoc_install_13,escidoc_global -Dinstall.home=target/escidoc -Dskip.install.first=false
--- runtrunk.sh
+++ runtrunk.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cd measurement
-mvn clean verify -Pskip_escidoc_install_trunk,escidoc_global -Dinstall.home=target/escidoc -Dskip.install.first=false
+#cd measurement
+mvn -f measurement/pom.xml clean verify -Pskip_escidoc_install_trunk,escidoc_global -Dinstall.home=target/escidoc -Dskip.install.first=false
